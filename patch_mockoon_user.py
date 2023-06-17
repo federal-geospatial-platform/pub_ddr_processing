33bfcf80ef7f848750ea4a4ff5da0278a643c108
Dan-Eli <bergeronpilon@gmail.com>
2023-06-16 22:03:20 -0400

    Add patch_mockoon.py
    File needed to patch the SQL Lite DB of QGIS

---
+++ patch_mockoon_user.py
@@ -0,0 +1,10 @@
+from qgis.core import  QgsAuthMethodConfig, QgsApplication
+cfg = QgsAuthMethodConfig()
+cfg.setMethod("Basic")
+cfg.setName("mergin4")
+cfg.setConfig("username", "pil123456")
+cfg.setConfig("password", "a123456")
+cfg.setId("p3m9sdd")
+auth_manager = QgsApplication.authManager()
+auth_manager.storeAuthenticationConfig(cfg)
+cfg.id()
---
+++ pub_ddr_processing/patch_mockoon_user.py
@@ -0,0 +1,10 @@
+from qgis.core import  QgsAuthMethodConfig, QgsApplication
+cfg = QgsAuthMethodConfig()
+cfg.setMethod("Basic")
+cfg.setName("mfrthn8")
+cfg.setConfig("username", "pil123456")
+cfg.setConfig("password", "a123456")
+cfg.setId("p7h9tdd")
+auth_manager = QgsApplication.authManager()
+auth_manager.storeAuthenticationConfig(cfg)
+cfg.id()
