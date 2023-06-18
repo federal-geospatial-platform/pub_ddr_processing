5f11a53e60859d36aed41634cd2519ba3cc8f94f
Daniel Pilon <Dan-Eli@users.noreply.github.com>
2023-06-17 21:01:09 -0400

    Update patch_mockoon_user.py

--- pub_ddr_processing/patch_mockoon_user.py
+++ pub_ddr_processing/patch_mockoon_user.py
@@ -2,7 +2,7 @@ from qgis.core import  QgsAuthMethodConfig, QgsApplication
 cfg = QgsAuthMethodConfig()
 cfg.setMethod("Basic")
 cfg.setName("mfrthn8")
-cfg.setConfig("username", "pil123456")
+cfg.setConfig("username", "mfrthn8")
 cfg.setConfig("password", "a123456")
 cfg.setId("p7h9tdd")
 auth_manager = QgsApplication.authManager()
