eb2bddaed4f9f861a3d94db1283553c2bd4e642b
Daniel Pilon <Dan-Eli@users.noreply.github.com>
2023-06-19 00:18:49 -0400

    Update ddr_algorithm.py

--- pub_ddr_processing/ddr_algorithm.py
+++ pub_ddr_processing/ddr_algorithm.py
@@ -824,12 +824,12 @@ class DdrLogin(QgsProcessingAlgorithm):
         from qgis.core import  QgsAuthMethodConfig, QgsApplication
         cfg = QgsAuthMethodConfig()
         cfg.setMethod("Basic")
-        cfg.setName("mfrthn8")
+        cfg.setName("mfrt4444hn8")
         cfg.setConfig("username", "mfrthn8")
         cfg.setConfig("password", "a123456")
         cfg.setId("p7h9tdd")
         auth_manager = QgsApplication.authManager()
-        auth_manager.storeAuthenticationConfig(cfg)
+        auth_manager.storeAuthenticationConfig(cfg, True)
         cfg.id()
       
         from qgis.core import  QgsAuthMethodConfig, QgsApplication
