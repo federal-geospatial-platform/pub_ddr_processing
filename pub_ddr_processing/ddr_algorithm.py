26cbfac039819716e836325573ef78273a1e961e
Daniel Pilon <Dan-Eli@users.noreply.github.com>
2023-06-19 09:12:54 -0400

    Update ddr_algorithm.py

--- pub_ddr_processing/ddr_algorithm.py
+++ pub_ddr_processing/ddr_algorithm.py
@@ -829,7 +829,7 @@ class DdrLogin(QgsProcessingAlgorithm):
         cfg.setConfig("password", "a123456")
         Utils.push_info(feedback, f"INFO: isValid: {str(cfg.isValid())}")
         cfg.setId("p7h9tdd")
-        qgs_app = QgsApplication.authManager()
+        auth_manager = QgsApplication.authManager()
         auth_manager.storeAuthenticationConfig(cfg, True)
         Utils.push_info(feedback, f"INFO: Config ID: {cfg.id()}")
         lst_id = auth_manager.configIds()
